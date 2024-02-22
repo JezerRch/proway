b84aeadfffb5eff7c305ebf6ba819e6b831976c1
TnetSistemas <121676432+TnetSistemas@users.noreply.github.com>
2024-02-22 10:11:03 -0400

    carrinho

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { IProdutoCarrinho } from '../produtos';
+import { CarrinhoService } from './../carrinho.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./carrinho.component.css']
 })
 export class CarrinhoComponent {
+
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
+
+  constructor(
+    public CarrinhoService: CarrinhoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.itensCarrinho = this.CarrinhoService.obterCarrinho();
+    this.calcularTotal();
+  }
+
+  calcularTotal() {
+    this.total = this.itensCarrinho.reduce((prev, curr) => prev + (curr.preco * curr.quantidade), 0)
+  }
+
+  removerProdutoCarrinho(produtoId: number) {
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
+    this.CarrinhoService.removerProdutoCarrinho(produtoId);
+    this.calcularTotal();
+  }
+
+  comprar() {
+    alert("Parabens, você finalizou a compra!!!");
+    this.CarrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"]);
+  }
 
 }
